8c8079ebeddedeb5d5ffd519ef4dba728df4f73d
alexm-hc <amogilevski@housecanary.com>
2019-10-28 16:39:47 -0700

    fixed sorting order

--- webcrawler.py
+++ webcrawler.py
@@ -4,7 +4,7 @@ import concurrent.futures
 import requests
 import sys
 
-class HTMLParser(HTMLParser):
+class Parser(HTMLParser):
   def __init__(self):
     HTMLParser.__init__(self)
     self.links = []
@@ -17,42 +17,31 @@ class HTMLParser(HTMLParser):
   def get_links(self):
     return self.links
 
-def get_links(URL):
+def get_links(URL, URL_links):
   URL_request = requests.get(URL)
-  parser = HTMLParser()
+  parser = Parser()
   parser.feed(str(URL_request.content))
-  URL_links = set(parser.get_links())
-  
+  URL_links[URL] = set(parser.get_links())
   return URL_links
 
-def print_links(URL, URL_links):
-  print(URL)
-  for link in URL_links:
-    print("  " + link)	
+def print_links(URL_links):
+  for URL_key, link_set in URL_links.items():
+    print(URL_key)
+    for link in link_set:
+      print("  " + link)	
 
 if __name__ == '__main__':
   if(len(sys.argv) != 2):
     print("Invalid number of arguments, please enter a single URL for crawling")
     sys.exit()
   URL = sys.argv[1]  
-  URL_links = get_links(URL)
-  print_links(URL, URL_links)
+  URL_links = {}
+  URL_links = get_links(URL, URL_links)
+  print_links(URL_links)
   workers = 4
   
   with concurrent.futures.ThreadPoolExecutor(max_workers = workers) as executor:
-    URL_future = {executor.submit(get_links, link): link for link in URL_links}
+    URL_future = {executor.submit(get_links, link, URL_links): link for link in URL_links[URL]}
     for future in concurrent.futures.wait(URL_future):
-      future.result()
-	
-    #URL_future = {executor.map(get_links, link): link for link in URL_links}
-    #for future in concurrent.futures.wait(URL_future):
-    #  print_links(link, URL_future)
-    #  link = URL_future[future]
-    #  try:
-    #    data = future.result()
-    #  except Exception as exc:
-    #    print('%r generated an exception: %s' % (link, exc))
-    #  else:
-    #    print(data)
-    #for link in URL_links:
-    # print_links(link, URL_links)
+      for item in future:
+        print_links(item.result())
